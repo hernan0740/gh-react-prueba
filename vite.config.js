dd5360e767a4bcc6c63e81b0ba81192bde5bd30e
Hernan Chapid <hernan0740@gmail.com>
2022-06-17 09:48:11 -0500

    fix bug2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://hernan0740.github.io/',
+  base: 'https://hernan0740.github.io/gh-react-prueba/',
   plugins: [react()]
 })
